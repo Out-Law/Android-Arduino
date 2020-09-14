d6279b25e68eb868b8afee431816d424a959545a
Nikita <nik.safronov.2014@bk.ru>
2020-09-14 15:22:16 +0300

    Add  ChartProgressBar

--- app/src/main/java/com/vk/app_arduino/CurrentTasksAtHome.java
+++ app/src/main/java/com/vk/app_arduino/CurrentTasksAtHome.java
@@ -1,5 +1,6 @@
 package com.vk.app_arduino;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+import com.hadiidbouk.charts.BarData;
+import com.hadiidbouk.charts.ChartProgressBar;
+
+
+
+import java.util.ArrayList;
 
 public class CurrentTasksAtHome extends Fragment {
+
+    ChartProgressBar mChart;
 
     public static CurrentTasksAtHome newInstance(){
         return new CurrentTasksAtHome();
@@ -23,6 +33,35 @@ public class CurrentTasksAtHome extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_current_tasks_at_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_current_tasks_at_home, container, false);
+
+        ArrayList<BarData> dataList = new ArrayList<>();
+
+        BarData data = new BarData("Sep", 3.4f, "3.4€");
+        dataList.add(data);
+
+        data = new BarData("Oct", 8.0f, "8.0€");
+        dataList.add(data);
+
+        data = new BarData("Nov", 1.8f, "1.8€");
+        dataList.add(data);
+
+        data = new BarData("Dec", 7.3f, "7.3€");
+        dataList.add(data);
+
+        data = new BarData("Jan", 6.2f, "6.2€");
+        dataList.add(data);
+
+        data = new BarData("Feb", 3.3f, "3.3€");
+        dataList.add(data);
+
+        mChart = (ChartProgressBar) view.findViewById(R.id.ChartProgressBar);
+
+        mChart.setDataList(dataList);
+        mChart.build();
+        mChart.setOnBarClickedListener(index -> Toast.makeText(getActivity(), String.valueOf(index), Toast.LENGTH_SHORT).show());
+        mChart.disableBar(dataList.size());
+        
+        return view;
     }
 }
